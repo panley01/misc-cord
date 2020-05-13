0e60c11f6f80b1ed298e378f8db5c4c454ee8a45
panley01 <52224740+panley01@users.noreply.github.com>
2020-05-13 12:59:29 +0100

    Added ChannelOverwrites class, fixed Permissions.

--- misccord/permissions.py
+++ misccord/permissions.py
@@ -1,9 +1,7 @@
-"""Utilities for working with Discord permissions."""
+"""Utilities for working with user flags."""
 import inspect
 from enum import Enum
 from typing import Any, Dict, List, Optional, Tuple
-
-# THIS SCRIPT IS CURRENTLY NOT IN A RUNNING STATE AND SHOULD NOT BE IMPORTED
 
 CREATE_INSTANT_INVITE = 1
 KICK_MEMBERS = 1 << 1
@@ -43,20 +41,20 @@ TEXT_DENIED = [KICK_MEMBERS,BAN_MEMBERS,ADMINISTRATOR,MANAGE_GUILD,VIEW_AUDIT_LO
 class PermissionType(Enum):
     """An enum representing the 4 Discord objects that have permissions."""
 
-    TEXT_CHANNEL = "text channel"
-    VOICE_CHANNEL = "voice channel"
-    ROLE = "role"
-    USER = "user"
+    text_channel = "text channel"
+    voice_channel = "voice channel"
+    role = "role"
+    user = "user"
 
     def typecheck(self,permissions) -> bool:
         """
         Checks whether a permission integer is valid for the selected PermissionType
         """
-        if self.TEXT_CHANNEL:
+        if self.text_channel:
             return all(map(lambda x: (permissions & x) != x, TEXT_DENIED))
-        elif self.VOICE_CHANNEL:
+        elif self.voice_channel:
             return all(map(lambda x: (permissions & x) != x, VOICE_DENIED))
-        elif self.ROLE or self.USER:
+        elif self.role or self.user:
             return True
         else:
             raise Exception("Invalid PermissionType")
@@ -71,81 +69,101 @@ class Permissions:
     -----------
     list: :class:`list`
         A python list object of all permissions enabled.
+    int: :class:`int`
+        A python integer of the perms value.
     """
 
-    def __init__(self, allow: int, deny=0: int, permtype: PermissionType) -> None:
+    def __init__(self, perms: int, permtype: PermissionType) -> None:
         """Create a new Flags instance."""
-        if not (isinstance(allow, int) or isinstance(allow, int)):
+        if not isinstance(perms, int):
             raise ValueError("permissions must be an integer from the Discord API.")
         if not isinstance(permtype, PermissionType):
             raise ValueError("permtype must be a PermissionType Object.")
-        if not (permtype.typecheck(allow) or permtype.typecheck(deny)):
+        if not permtype.typecheck(perms):
             raise Exception("permissions integer contains permissions invalid for the selected permtype")
-        self.allow = allow
-        self.deny = deny
+        self.perms = perms
+        self.type = permtype
 
-        @property
-        def set(self) -> List[Tuple[str, bool]]:
-            """
-            Return a list of permissions on the value.
-            True if the perm is allowed, False if denied
-            :rtype: list[tuple[str, bool]]
-            """
-            perms = []
+    def enabled(self) -> List[Tuple[str, bool]]:
+        """
+        Return a list of enabled permissions on the value.
+        :rtype: list[tuple[str, bool]]
+        """
+        perms = []
 
-            for name, value in inspect.getmembers(self):
-                if name.startswith("_"):
-                    continue
+        for name, value in inspect.getmembers(self):
+            if name.startswith("_"):
+                continue
 
-                if isinstance(value, bool):
+            if isinstance(value, bool):
+                if value:
                     perms.append(name)
 
-            return perms
-        
-        @property
-        def allow_list(self) -> List[Tuple[str, bool]]:
-            """
-            Placeholder.
-            :rtype: list[tuple[str, bool]]
-            """
+        return perms
+
+    def __iter__(self) -> List[Tuple[str, bool]]:
+        """
+        Return a list of enabled permissions on the value.
+        Under the hood this just calls the enabled method.
+        :rtype: list[tuple[str, bool]]
+        """
+        for perm in self.enabled():
+            yield perm
+    
+    def __int__(self):
+        """
+        Return the Int value for the permissions.
+        Under the hood this just calls self.perms.
+        :rtype: int()
+        """
+        return self.perms
+
+    # Permission bitwise checks
+
+    @property
+    def stream(self) -> bool:
+        """Stream Permission | VC ONLY."""
+        return (self.perms & STREAM) == STREAM
+
+class ChannelOverwrites():
+    """
+    Permission overwrites from a Discord channel, targets one passed role or user.
+    Represents the channel overwrites permissions returned from the Discord API
+    in integer form as a class with attributes representing each permission in
+    both the allow and deny fields.
+    Attributes
+    -----------
+    list: :class:`list`
+        A list of tuples from both the allowed and denied perms in the format
+        (str, bool). Allowed perms are True, and denied are False.
+    allow: :class:`Permissions`
+        A Permissions object for the permissions int passed to it, represents allow overwrites.
+    deny: :class:`Permissions`
+        A Permissions object for the permissions int passed to it, represents deny overwrites.
+    
+    """
+
+    def __init__(self, allow: int =0, deny: int =0, permtype: PermissionType):
+        if not isinstance(allow, int):
+            raise ValueError("allow must be an integer from the Discord API.")
+        if not isinstance(deny, int):
+            raise ValueError("deny must be an integer from the Discord API.")
+        if not isinstance(permtype, PermissionType):
+            raise ValueError("permtype must be a PermissionType Object.")
+        if (permtype == PermissionType.user) or (permtype == PermissionType.role):
+            raise Exception("permtype invalid for ChannelOverwrites. Must be PermissionType.text or PermissionType.voice.")
+        if not permtype.typecheck(perms):
+            raise Exception("permissions integer contains permissions invalid for the selected permtype")
+        self.allow = Permissions(allow,permtype)
+        self.deny = Permissions(deny,permtype)
+        self.type = permtype
+
+        def __iter__(self) -> List[Tuple[str, bool]]:
             perms = []
 
-            for name, value in inspect.getmembers(self):
-                if name.startswith("_"):
-                    continue
-
-                if isinstance(value, bool):
-                    if value:
-                        perms.append(name)
-
+            for perm in list(self.allow):
+                perms.append(tuple(perm, True))
+            for perm in list(self.deny):
+                perms.append(tuple(perm, False))
+            
             return perms
-        
-        @property
-        def deny_list(self) -> List[Tuple[str, bool]]:
-            """
-            Placeholder.
-            :rtype: list[tuple[str, bool]]
-            """
-            perms = []
-
-            for name, value in inspect.getmembers(self):
-                if name.startswith("_"):
-                    continue
-
-                if isinstance(value, bool):
-                    if not value:
-                        perms.append(name)
-
-            return perms
-
-        # Permission bitwise checks
-
-        @property
-        def stream(self) -> bool:
-            """Discord perm placeholder"""
-            if (self.allow & STREAM) == STREAM:
-                return True
-            elif (self.deny & STREAM) == STREAM:
-                return False
-            else:
-                return None
